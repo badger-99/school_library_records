9a7c8ff651c277d662b04d762854dbd134bed534
Alfred M <alfred.mkg@proton.me>
2023-09-18 17:59:38 +0300

    Add person class

--- person.rb
+++ person.rb
@@ -1,1 +1,19 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
 
+  def initialize(name='Unknown', age, parent_permission=true)
+    @id = Random.rand(1..80000)
+    @name = name
+    @age = age
+  end
+
+  def can_use_services?
+    return true if of_age
+  end
+
+  private
+    def of_age?
+      return true if @age >=18
+    end
+end
