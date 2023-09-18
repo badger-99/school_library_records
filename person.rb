0d6f5d818ec040f3a9564821e3ed4b96e91a4bea
Alfred M <alfred.mkg@proton.me>
2023-09-18 18:24:14 +0300

    Correct Rubocop offenses

--- person.rb
+++ person.rb
@@ -2,19 +2,20 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(name = 'Unknown', age, parent_permission = true)
-    @id = Random.rand(1..80000)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..80_000)
     @name = name
     @age = age
     @parent_permissioin = parent_permission
   end
 
   def can_use_services?
-    return true if of_age
+    true if of_age
   end
 
   private
-    def of_age?
-      return true if @age >=18 || @parent_permission
-    end
+
+  def of_age?
+    true if @age >= 18 || @parent_permission
+  end
 end
