5919592441893dea465f83fd66d31ae1fe8c830c
Alfred M <alfred.mkg@proton.me>
2023-09-19 17:56:28 +0300

    Add base decorator class

--- person.rb
+++ person.rb
@@ -25,3 +25,16 @@ class Person < Nameable
     true if @age >= 18
   end
 end
+
+class Decorator < Nameable
+  attr_accessor :nameable_object
+
+  def initialize(nameable_object)
+    @nameable_object = nameable_object
+  end
+
+  def correct_name
+    @nameable_object.correct_name
+  end
+end
+
