f8974e1dbd520fa38d1a7110f745389dea4fe086
Alfred M <alfred.mkg@proton.me>
2023-09-20 19:35:43 +0300

    Set student-class association

--- student.rb
+++ student.rb
@@ -3,7 +3,12 @@ require './person'
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
+    set_classroom(classroom)
+  end
+
+  def set_classroom(classroom)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hockey
