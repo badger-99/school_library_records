5e11b9541a054bdfa0b3fb1e78bd8e3aa7661e9e
Alfred M <alfred.mkg@proton.me>
2023-09-20 20:57:29 +0300

    Test Student and Person objects

--- main.rb
+++ main.rb
@@ -2,3 +2,17 @@ require './person'
 require './student'
 require './teacher'
 require './classroom'
+require './capitalize'
+require './trimmer'
+
+s_class = Classroom.new('S-Class')
+ichigo = Student.new(s_class, '18', 'Ichigo')
+puts s_class.students.first.name
+puts ichigo.classroom.label
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
--- person.rb
+++ person.rb
@@ -1,6 +1,4 @@
 require './nameable'
-require './capitalize'
-require './trimmer'
 
 class Person < Nameable
   attr_accessor :name, :age
@@ -28,10 +26,3 @@ class Person < Nameable
     true if @age >= 18
   end
 end
-
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
