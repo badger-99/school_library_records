c57c56d5306c4106e62e0de3ef9da7462db30592
Alfred M <alfred.mkg@proton.me>
2023-09-20 20:57:01 +0300

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,12 +1,14 @@
 require './person'
 
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
-    set_classroom(classroom)
+  attr_reader :classroom
+  
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission:)
+    assign_classroom(classroom)
   end
 
-  def set_classroom(classroom)
+  def assign_classroom(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
