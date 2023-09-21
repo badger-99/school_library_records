45e85ad11e05f796560d7c12036ad93e594ac7e4
Alfred M <alfred.mkg@proton.me>
2023-09-21 18:54:10 +0300

    Update Rental class to return book title & author

--- rental.rb
+++ rental.rb
@@ -6,9 +6,17 @@ class Rental
     @date = date
 
     @person = person
-    person.rental << self
+    person.add_rental(self) if person.respond_to?(:add_rental)
 
     @book = book
-    book.rental << self
+    book.add_rental(self) if book.respond_to?(:add_rental)
+  end
+
+  def title
+    @book.retrieve_title
+  end
+
+  def author
+    @book.retrieve_author
   end
 end
