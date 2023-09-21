aa4c030c536151da38cd609324e70e8bff09a87d
Alfred M <alfred.mkg@proton.me>
2023-09-21 15:17:40 +0300

    Testing student methods

--- main.rb
+++ main.rb
@@ -1,18 +1,30 @@
-require './person'
-require './student'
-require './teacher'
-require './classroom'
-require './capitalize'
-require './trimmer'
+require './app'
 
-s_class = Classroom.new('S-Class')
-ichigo = Student.new(s_class, '18', 'Ichigo')
-puts s_class.students.first.name
-puts ichigo.classroom.label
+app = App.new()
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+app.create_student(18, 'Ichigo', true)
+app.create_student(16, 'Tanjiro', false)
+list = app.retrieve_students
+puts list[1].name
+
+
+
+
+# require './person'
+# require './student'
+# require './teacher'
+# require './classroom'
+# require './capitalize'
+# require './trimmer'
+
+# s_class = Classroom.new('S-Class')
+# ichigo = Student.new(s_class, '18', 'Ichigo')
+# puts s_class.students.first.name
+# puts ichigo.classroom.label
+
+# person = Person.new(22, 'maximilianus')
+# puts person.correct_name
+# capitalized_person = CapitalizeDecorator.new(person)
+# puts capitalized_person.correct_name
+# capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+# puts capitalized_trimmed_person.correct_name
