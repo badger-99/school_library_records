6d16cd1da8be2f0ac7ff2b83118f52677023c5cb
Alfred M <alfred.mkg@proton.me>
2023-09-21 15:12:35 +0300

    Fix typo in Book initializer

--- book.rb
+++ book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :title, :author
   attr_reader :rental
 
-  def intiialize(title, author)
+def initialize(title, author)
     @title = title
     @author = author
     @rental = []
