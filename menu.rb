acef05e63a7d5eedbca3ff89d24c7d15e6bc68a2
Alfred M <alfred.mkg@proton.me>
2023-09-22 16:03:00 +0300

    Add main-menu module

---
+++ menu.rb
@@ -0,0 +1,16 @@
+module MainMenu
+  @menu = "  Please choose an optionby entering a number.
+  1 - List all books
+  2 - List all people
+  3 = Create a person
+  4 - Create a book
+  5 - Create a rental
+  6 - List all rentals for a given person ID
+  7 - Exit"
+  def print_menu
+    puts @menu
+  end
+end
+
+# include Menu
+# Menu.print_menu
