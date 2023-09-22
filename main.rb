a9f58d521a8ae96dc8417aeba9f4edb4924ed09c
Alfred M <alfred.mkg@proton.me>
2023-09-22 23:21:18 +0300

    Resolve rubocop offenses

--- main.rb
+++ main.rb
@@ -13,7 +13,6 @@ module Main
       puts MainMenu.display_menu
       puts "\n"
       selection = gets.chomp.to_i
-      next
 
     when 1 # List all books
       books = app.retrieve_books
@@ -22,7 +21,6 @@ module Main
       end
       selection = 0
       puts "\n"
-      next
 
     when 2 # List all people
       people = app.retrieve_people
@@ -31,62 +29,55 @@ module Main
       end
       selection = 0
       puts "\n"
-      next
 
     when 3 # Create a person
       print 'Do you want to create a Student(1) or a Teacher(2)? [Input the number]: '
       choice = gets.chomp.to_i
+      age = name = permission = specialization = ''
+      allowed = student = teacher = nil
 
       if choice == 1
+        student = true
         print 'Age: '
         age = gets.chomp.to_i
         print 'Name: '
         name = gets.chomp
         print 'Has the parent(s) given permission? [Y/N]: '
         permission = gets.chomp.downcase
-        allowed = nil
-
-        if permission == 'y'
-          allowed = true
-        elsif permission == 'n'
-          allowed = false
-        end
-
-        if allowed.nil?
-          puts 'Select a valid choice for parental permission'
-          selection = 3
-        elsif !age.is_a?(Integer) || age < 1
-          puts 'Enter positive numbers only for the age.'
-          selection = 3
-        else
-          app.create_student(age, name, allowed)
-          puts 'Student has been created successfully'
-          selection = 0
-        end
-
       elsif choice == 2
+        teacher = true
         print 'Age: '
         age = gets.chomp.to_i
         print 'Name: '
         name = gets.chomp
         print 'Specialization: '
         specialization = gets.chomp
-        if !age.is_a?(Integer) || age < 1
-          puts 'Enter positive numbers only for the age.'
-          selection = 3
-        else
-          app.create_teacher(age, name, specialization)
-          puts 'Teacher has been created successfully'
-          selection = 0
-        end
-
       else
         puts 'Select a valid Person option.'
         selection = 3
       end
 
+      # input validation
+      allowed = true if permission == 'y'
+      allowed = false if permission == 'n'
+
+      if student && !allowed.nil? && age.positive?
+        app.create_student(age, name, allowed)
+        puts 'Student has been created successfully'
+        selection = 0
+      elsif student && allowed.nil?
+        puts 'Select a valid choice for parental permission'
+        selection = 3
+      elsif teacher && age.positive?
+        app.create_teacher(age, name, specialization)
+        puts 'Teacher has been created successfully'
+        selection = 0
+      elsif (student || teacher) && age < 1
+        puts 'Enter positive numbers only for the age.'
+        selection = 3
+      end
+
       puts "\n"
-      next
 
     when 4 # Create a book
       print 'Title: '
@@ -97,11 +88,10 @@ module Main
       selection = 0
       puts 'Book has been created successfully'
       puts "\n"
-      next
 
     when 5 # Record a rental
       # get date
-      date_pattern = /\A\d{4}\/\d{2}\/\d{2}\z/
+      date_pattern = %r{\A\d{4}/\d{2}/\d{2}\z}
       print 'Date [YYYY/MM/DD]: '
       date = gets.chomp
 
@@ -110,17 +100,17 @@ module Main
         puts 'Select a book from the following list by number'
         books_list = app.retrieve_books
         books_list.each_with_index do |book, index|
-          puts "#{index+1} - #{book.title}"
+          puts "#{index + 1} - #{book.title}"
         end
-        book_selection = gets.chomp.to_i-1
+        book_selection = gets.chomp.to_i - 1
 
         # select book
         puts 'Select a person from this list by number'
         people_list = app.retrieve_people
         people_list.each_with_index do |person, index|
-          puts "#{index+1} - #{person.name}  #{person.class}"
+          puts "#{index + 1} - #{person.name}  #{person.class}"
         end
-        person_selection = gets.chomp.to_i-1
+        person_selection = gets.chomp.to_i - 1
 
         # create the rental record
         person = people_list[person_selection]
@@ -133,19 +123,15 @@ module Main
         selection = 5
       end
       puts "\n"
-      next
 
     when 6 # List all rentals by ID
       print 'ID of person: '
       id = gets.chomp.to_i
       rental_records = []
       rentals = app.retrieve_rentals
-
       rentals.each do |rental|
-        if rental.id == id
-          rental_records.push(rental)
+        rental_records.push(rental) if rental.id == id
       end
-
       if rental_records
         rental_records.each do |rental|
           puts "Date: #{rental.date}, Book \"#{rental.title}\" by #{rental.author}"
@@ -153,17 +139,16 @@ module Main
       else
         puts 'The person with the ID number you provided has not made any rentals.'
       end
-      
+
       puts "\n"
       selection = 0
-      next
 
     when 7
       selection = 7
-      next
     else
-      # handle wrong selection here
+      selection = 0
     end
+    next
   end
   puts 'Thank you for using the School Library App'
 end
