b52a0a5dace620f12b6ff8c4015efa99571c4573
Alfred M <alfred.mkg@proton.me>
2023-09-22 19:26:44 +0300

    Reduce max id number to 1000 in person.rb

--- person.rb
+++ person.rb
@@ -6,7 +6,7 @@ class Person < Nameable
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = Random.rand(1..80_000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
