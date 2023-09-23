90e2364d8fad6324a8c15af43dbc7ac727fbdf6d
Alfred M <alfred.mkg@proton.me>
2023-09-23 15:20:50 +0300

    Update main menu

--- menu.rb
+++ menu.rb
@@ -1,10 +1,10 @@
 module MainMenu
-  @menu = "  Please choose an optionby entering a number.
+  @menu = "  Please choose an option by entering a number.
   1 - List all books
   2 - List all people
-  3 = Create a person
-  4 - Create a book
-  5 - Create a rental
+  3 = Register a person
+  4 - Register a book
+  5 - Record a rental
   6 - List all rentals for a given person ID
   7 - Exit"
 
