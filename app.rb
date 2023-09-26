08a42a345f2e56c4810e88bc98880edb9cc23127
Henok Kirubel <henokhm2@gmail.com>
2023-09-26 23:54:05 +0300

    Add save_to_file methods to PersonsManager, BooksManager and RentalsManager

--- app.rb
+++ app.rb
@@ -28,6 +28,18 @@ class PersonsManager
     @teachers_list.push(teacher)
     teacher
   end
+
+  def save_to_file
+    students_data = students_list.map { |student| student.to_json }
+    File.open('students.json', 'w') do |file|
+      file.puts JSON.generate(students_data)
+    end
+
+    teachers_data = teachers_list.map { |teacher| teacher.to_json }
+    File.open('teachers.json', 'w') do |file|
+      file.puts JSON.generate(teachers_data)
+    end
+  end
 end
 
 class BooksManager
@@ -41,6 +53,13 @@ class BooksManager
     book = Book.new(title, author)
     @books_list.push(book)
     book
+  end
+
+  def save_to_file
+    books_data = books_list.map { |book| book.to_json }
+    File.open('books.json', 'w') do |file|
+      file.puts JSON.generate(books_data)
+    end
   end
 end
 
@@ -59,9 +78,10 @@ class RentalsManager
     rental
   end
 
-  def save_rental_to_file
-    File.open('rental.json', 'w') do |file|
-      file.puts to_json
+  def save_to_file
+    rentals_data = rentals_list.map { |rental| rental.to_json }
+    File.open('rentals.json', 'w') do |file|
+      file.puts JSON.generate(rentals_data)
     end
   end
 end
