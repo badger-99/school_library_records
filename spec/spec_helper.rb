f563ab70e1b1a9633f4f4d2eb7ba65a58f5bca29
Alfred M <alfred.mkg@proton.me>
2023-09-28 08:23:37 +0300

    Add spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require './project_files/book'
+require './project_files/classroom'
+require './project_files/person'
+require './project_files/teacher'
+require './project_files/student'
+require './project_files/rental'
